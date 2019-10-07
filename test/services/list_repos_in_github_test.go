75a64c58dbe87ee6d850112f3d2ca22819498a4f
Guilherme Esteves <guilhermesteves@icloud.com>
2019-10-06 21:49:36 -0300

    add dotenv to tests

--- test/services/list_repos_in_github_test.go
+++ test/services/list_repos_in_github_test.go
@@ -5,10 +5,13 @@ import (
 	"time"
 
 	"github.com/guilhermesteves/github-repos-sort-api/internal/services"
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestListRpostInGithub(t *testing.T) {
+	godotenv.Load()
+
 	result, err := services.ListReposInGithub("go", 1)
 
 	assert.Nil(t, err)
