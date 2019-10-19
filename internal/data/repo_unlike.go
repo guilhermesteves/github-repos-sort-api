7e03a26bebe0610d0f6f0dfd402f5ab5e2bb8de0
Guilherme Esteves <guilhermesteves@icloud.com>
2019-10-19 19:50:18 -0300

    wip

--- internal/data/repo_unlike.go
+++ internal/data/repo_unlike.go
@@ -1,7 +1,41 @@
 package data
 
-import "github.com/guilhermesteves/github-repos-sort-api/internal/model"
+import (
+	"context"
+	"time"
 
-func UnlikeRepoById(id string, params map[string]string) (*model.Repo, error) {
-	return &model.Repo{}, nil
+	"github.com/guilhermesteves/github-repos-sort-api/internal/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func UnlikeRepoById(id string, params map[string]string) (bool, error) {
+	client, err := utils.ConnectOnMongo()
+
+	if err != nil {
+		return false, err
+	}
+
+	db := client.Database(utils.MongoDbDatabase())
+	ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+
+	ip := params["ip"]
+
+	upsert := false
+
+	_, err = db.Collection("col_repo").UpdateOne(
+		ctx,
+		bson.M{"_id": id},
+		bson.M{
+			"$inc": bson.M{"likes": int64(-1)},
+			"$pull": bson.M{"likedBy": ip},
+		},
+		&options.UpdateOptions{Upsert: &upsert},
+	)
+
+	if err != nil {
+		return false, err
+	}
+
+	return false, nil
 }
